9f901a3e131cbff305d51be804039ce2a260098a
bardiel73 <76916600+bardiel73@users.noreply.github.com>
2026-03-09 21:48:59 +0300

    c includes

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include "nocrt.h" // IWYU pragma: keep
 #include <windows.h> // IWYU pragma: keep
 
+// Halal c includes
+#include <stdint.h> // IWYU pragma: keep
+#include <stddef.h> // IWYU pragma: keep
+#include <stdarg.h> // IWYU pragma: keep
+#include <stdbool.h> // IWYU pragma: keep
+#include <intrin.h> // IWYU pragma: keep
+
 __attribute((force_align_arg_pointer, used, noreturn))
 void entry_point() {
 
